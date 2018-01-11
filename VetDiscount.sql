b7ffc8e8c834ffcfe47621a6ea8781a97e9137c9
HunterKir <hunter.kiritsis@gmail.com>
2018-01-11 09:36:53 -0700

    revised database

--- VetDiscount.sql
+++ VetDiscount.sql
@@ -15,32 +15,43 @@ CREATE TABLE `store_type` (
 	`name` VARCHAR(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
-
-CREATE TABLE `store` (
+CREATE TABLE `company` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(255) NOT NULL,
-	`hours` VARCHAR(255),
-	`owner_id` INT(11) NOT NULL,
+	`owner_id` INT(11),
 	`type_id` INT(11),
-	`phone_number` VARCHAR(255),
 	`store_url` VARCHAR(255),
+	`chain_bool` TINYINT(1) DEFAULT 0,
 	FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`),
 	FOREIGN KEY (`type_id`) REFERENCES `store_type` (`id`),
 	PRIMARY KEY (`id`)
-);
 
+);
 CREATE TABLE `address` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`state` VARCHAR(2) NOT NULL,
 	`city` VARCHAR(255) NOT NULL,
-	`zip_code` INT(5),
+	`zip_code` INT(5) NOT NULL,
 	`street` VARCHAR(255) NOT NULL,
 	`lat` VARCHAR(255),
 	`long` VARCHAR(255),
-	`store_id` INT(11) NOT NULL,
-	FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
 	PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `location` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`hours` VARCHAR(255),
+	`owner_id` INT(11),
+	`phone_number` VARCHAR(255),
+	`company_id` INT(11) NOT NULL,
+	`address_id` INT(11) NOT NULL,
+	FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
+	FOREIGN KEY (`company_id`) REFERENCES `company` (`id`),
+	FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`),
+	PRIMARY KEY (`id`)
+);
+
+
 
 CREATE TABLE `discount` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
@@ -48,17 +59,26 @@ CREATE TABLE `discount` (
 	`start_date` VARCHAR(255),
 	`end_date` VARCHAR(255),
 	`info` TEXT,
-	`store_id` INT(11) NOT NULL,
-	FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
+	`user_id` INT(11) NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `favorite_list` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
-	`store_id` INT(11) NOT NULL,
+	`location_id` INT(11) NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
-	FOREIGN KEY (`store_id`) REFERENCES `store` (`id`),
+	FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `participating_locations` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`location_id` INT(11) NOT NULL,
+	`discount_id` INT(11) NOT NULL,
+	FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
+	FOREIGN KEY (`discount_id`) REFERENCES `discount` (`id`),
 	PRIMARY KEY (`id`)
 );
 
@@ -78,16 +98,22 @@ INSERT INTO store_type (name) VALUES ('Drugstore');
 
 
 INSERT INTO user (username, password, email) VALUES ('HunterK', 'password123', 'hunter@SD.com');
-INSERT INTO store (name, owner_id, type_id) VALUES ('Skill Distillery', '1', '1');
-INSERT INTO address (state, city, zip_code, street, store_id) VALUES ('CO', 'Denver', '80111', '7400 East Orchard Road', '1');
-INSERT INTO discount (amount, info, store_id) VALUES ('20% off', 'Vets get 20% off on all white board erasers!', '1');
+INSERT INTO company (name, owner_id, type_id) VALUES ('Skill Distillery', '1', '1');
+INSERT INTO address (state, city, zip_code, street) VALUES ('CO', 'Denver', '80111', '7400 East Orchard Road');
+INSERT INTO location (company_id, address_id) VALUES ('1', '1');
+INSERT INTO discount (amount, info, user_id) VALUES ('20% off', 'Vets get 20% off on all white board erasers!', '1');
+INSERT INTO participating_locations (location_id, discount_id) VALUES ('1', '1');
 
 INSERT INTO user (username, password, email) VALUES ('Aaron', '123', 'a@a.com');
-INSERT INTO store (name, owner_id, type_id) VALUES ('Barnes and Noble', '2', '2');
-INSERT INTO address (state, city, zip_code, street, store_id) VALUES ('FL', 'Orlando', '33467', '123 Main Street', '2');
-INSERT INTO discount (amount, info, store_id, start_date, end_date) VALUES ('Buy 1 get 1', 'You only get one. Pay full price.', '2', '01/10/2018', '01/22/2018');
+INSERT INTO company (name, owner_id, type_id) VALUES ('Barnes and Noble', '2', '2');
+INSERT INTO address (state, city, zip_code, street) VALUES ('FL', 'Orlando', '33467', '123 Main Street');
+INSERT INTO location (company_id, address_id) VALUES ('2', '2');
+INSERT INTO discount (amount, info, start_date, end_date, user_id) VALUES ('Buy 1 get 1', 'You only get one. Pay full price.', '01/10/2018', '01/22/2018', '2');
+INSERT INTO participating_locations (location_id, discount_id) VALUES ('2', '2');
 
 INSERT INTO user (username, password, email) VALUES ('baby', 'baby', 'babylikes2drink@bbb.com');
-INSERT INTO store (name, owner_id, type_id) VALUES ('Baby\'s Booze Boutique', '3', '3');
-INSERT INTO address (state, city, zip_code, street, store_id) VALUES ('CO', 'Denver', '80111', '1111 Tipsy Blvd.', '3');
-INSERT INTO discount (amount, info, store_id, start_date, end_date) VALUES ('15% off everything', 'Baby wants the bottle!', '3', '01/10/2018', '01/30/2018');
+INSERT INTO company (name, owner_id, type_id) VALUES ('Baby\'s Booze Boutique', '3', '3');
+INSERT INTO address (state, city, zip_code, street) VALUES ('CO', 'Denver', '80111', '1111 Tipsy Blvd.');
+INSERT INTO location (company_id, address_id) VALUES ('3', '3');
+INSERT INTO discount (amount, info, start_date, end_date, user_id) VALUES ('15% off everything', 'Baby wants the bottle!', '01/10/2018', '01/30/2018', '3');
+INSERT INTO participating_locations (location_id, discount_id) VALUES ('3', '3');

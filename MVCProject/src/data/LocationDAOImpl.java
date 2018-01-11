48acf1bfdd2c6427eb40fb16e34e167114771968
falcon <aaron.skilldistill@gmail.com>
2018-01-11 13:47:51 -0700

    create locationDAO/impl stub

---
+++ MVCProject/src/data/LocationDAOImpl.java
@@ -0,0 +1,39 @@
+package data;
+
+import java.util.List;
+
+import entities.Location;
+
+public class LocationDAOImpl implements LocationDAO {
+
+	@Override
+	public List<Location> index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Location show(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Location create(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Location update(String json, int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

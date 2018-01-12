f29c7daac36927e0d2384cbb33b4f57b456e84bb
HunterKir <hunter.kiritsis@gmail.com>
2018-01-12 12:48:04 -0700

    started work on navbar

--- MVCProject/WebContent/app/appModule/navbar/navbar.component.js
+++ MVCProject/WebContent/app/appModule/navbar/navbar.component.js
@@ -1,7 +1,14 @@
 angular.module('appModule').component('navbar', {
 	templateUrl : 'app/appModule/navbar/navbar.component.html',
 	controllerAs : 'vm',
-	controller : function() {
+	controller : function(vetService) {
 		var vm = this;
+		vm.distances = ['3', '5', '10', '10+']
+
+		vm.search = function(params) {
+			vetService.search(params).then(function(data) {
+
+			});
+		}
 	}
 });
--- MVCProject/WebContent/app/appModule/vet.service.js
+++ MVCProject/WebContent/app/appModule/vet.service.js
@@ -1,20 +1,20 @@
 angular.module('appModule').factory('vetService', function($http) {
 	var service = {};
-	
+
 	service.index = function() {
         return $http({
             method : 'GET',
             url : 'rest/location'
         });
     }
-	
+
 	service.search = function(searchTerm){
 		return $http({
 			method : 'GET',
-			url : 'rest/location/search' + searchTerm
+			url : 'rest/location/search/' + searchTerm
 		});
 	}
-	
+
 	
 	return service;
 })

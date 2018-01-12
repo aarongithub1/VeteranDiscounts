46e2f9929e10807aec54b90584f161164b36ae8d
Jake <jacobtaric@yahoo.com>
2018-01-12 11:52:24 -0700

    search function added to service

--- MVCProject/src/data/LocationDAOImpl.java
+++ MVCProject/src/data/LocationDAOImpl.java
@@ -18,96 +18,95 @@ import entities.Location;
 @Transactional
 @Repository
 public class LocationDAOImpl implements LocationDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	// index - show all locations
 	@Override
 	public Set<Location> index() {
 		String query = "SELECT DISTINCT l FROM Location l";
-		List<Location> locations = em.createQuery(query, Location.class)
-				.getResultList();
+		List<Location> locations = em.createQuery(query, Location.class).getResultList();
 		Set<Location> set = new HashSet<>(locations);
 		return set;
 	}
-	
+
 	// show a location by id
 	@Override
 	public Location show(int lid) {
-		
+
 		Location location = em.find(Location.class, lid);
-		
-		if(location == null || location.getId() != lid) {
+
+		if (location == null || location.getId() != lid) {
 			return null;
 		}
-		
+
 		return location;
 	}
-	
+
 	// create a location for a company
 	@Override
 	public Location create(String json, int cid) {
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		Location location = null;
-		
+
 		try {
-			
+
 			location = mapper.readValue(json, Location.class);
 			Company company = em.find(Company.class, cid);
 			location.setCompany(company);
 			em.persist(location);
 			em.flush();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return location;
-		
+
 	}
-	
+
 	// delete a location
 	@Override
 	public Boolean delete(int lid) {
 		String query = "DELETE FROM Location l WHERE l.id = :lid";
 		int delete = em.createQuery(query).setParameter("lid", lid).executeUpdate();
-		if(delete > 0) {
+		if (delete > 0) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	// update a location
 	@Override
 	public Location update(String json, int lid) {
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		Location updateLocation = null;
 		Location oldLocation = null;
-		
+
 		try {
 			updateLocation = mapper.readValue(json, Location.class);
 			oldLocation = em.find(Location.class, lid);
-			
+
 			oldLocation.setHours(updateLocation.getHours());
-//			oldLocation.setOwner(updateLocation.getOwner());
+			// oldLocation.setOwner(updateLocation.getOwner());
 			oldLocation.setPhoneNumber(updateLocation.getPhoneNumber());
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return oldLocation;
 	}
-	
-	//Event search by keyword
+
+	// Event search by keyword
 	@Override
 	public List<Location> getAllLocationsByKeyword(String keyword) {
 		String query = "SELECT l FROM Location l WHERE l.company.name"
-				+ " LIKE CONCAT('%', :keyword,'%')"
-				+ " OR l.company.name LIKE CONCAT('%', :company,'%')"
+				+ " LIKE CONCAT('%', :company,'%')"
+				+ " OR l.phoneNumber LIKE CONCAT('%', :phoneNumber,'%')"
 				+ " OR l.address.street LIKE CONCAT('%', :street,'%')"
 				+ " OR l.address.city LIKE CONCAT('%', :city,'%')"
 				+ " OR l.address.state LIKE CONCAT('%', :state,'%')"

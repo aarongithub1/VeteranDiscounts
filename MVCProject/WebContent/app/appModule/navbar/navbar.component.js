3f68d33974a3d433827be796fb4b2be49075cd93
Jake <jacobtaric@yahoo.com>
2018-01-12 13:57:43 -0700

    if you put nothing into search it returns all values

--- MVCProject/WebContent/app/appModule/navbar/navbar.component.js
+++ MVCProject/WebContent/app/appModule/navbar/navbar.component.js
@@ -4,10 +4,11 @@ angular.module('appModule').component('navbar', {
 	controller : function(vetService) {
 		var vm = this;
 		vm.distances = ['3', '5', '10', '10+']
-
-		vm.search = function(params) {
-			vetService.search(params).then(function(data) {
-
+		vm.searchTerm = "";
+		
+		vm.search = function() {
+			vetService.search(vm.searchTerm).then(function(data) {
+				console.log(data.data);
 			});
 		}
 	}

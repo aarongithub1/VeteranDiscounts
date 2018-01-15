79069e33f026f850198619008780e69a3e58a626
Jake <jacobtaric@yahoo.com>
2018-01-15 10:59:03 -0700

    got active selection into scope

--- MVCProject/WebContent/app/appModule/results/results.component.js
+++ MVCProject/WebContent/app/appModule/results/results.component.js
@@ -1,7 +1,7 @@
 angular.module('appModule').component('results', {
 	templateUrl : 'app/appModule/results/results.component.html',
 	controllerAs : 'vm',
-	controller : function(vetService,$scope) {
+	controller : function(vetService,$scope,$rootScope) {
 		var vm = this;
 		vm.results = [];
 		vm.searchTerm = null;
@@ -9,12 +9,14 @@ angular.module('appModule').component('results', {
 		
 		vm.makeActive = function(result){
 			vm.active = result;
+			$rootScope.$broadcast('activeSelection', vm.active);
 		}	
 		
 		$scope.$on('search-event', function(e,args){
-			console.log('scope hit');
+			//console.log('scope hit in results');
 			vm.results = args.searchResults;
-			vm.active = vm.results[0];
+			vm.makeActive(vm.results[0]);
 		})
+		
 	}
 });

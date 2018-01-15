909ddbb0992ad25fef46c4c48a1bd73195b1f788
Jake <jacobtaric@yahoo.com>
2018-01-15 14:58:58 -0700

    phone number parsed and displays only when avaliable

--- MVCProject/WebContent/app/appModule/location/location.component.js
+++ MVCProject/WebContent/app/appModule/location/location.component.js
@@ -4,11 +4,23 @@ angular.module('appModule').component('location', {
 	controller : function($scope) {
 		var vm = this;
 		vm.location = null;
+		vm.phoneNumber = null;
+		
+		function phoneNumberFormater(num){
+			vm.phoneNumber = '(' + num.slice(0,3)
+			+ ')' + num.slice(3,7) + '-' + num.slice(7,10);
+		}
 		
 		$scope.$on('activeSelection', function(e,arg){
 			//console.log('scope hit in discount');
-			console.log(arg);
+			//console.log(arg);
 			vm.location = arg;
+			if(arg.phoneNumber != null && arg.phoneNumber.length === 10){
+				phoneNumberFormater(arg.phoneNumber);
+			} else {
+				vm.phoneNumber = null;
+			}
+			
 		});
 	}
 });

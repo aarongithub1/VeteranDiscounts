fed0851811596524dacfdda86bb89735c8a0f7c9
HunterKir <hunter.kiritsis@gmail.com>
2018-01-15 11:37:56 -0700

    fix merge conflict

---
+++ MVCProject/WebContent/app/appModule/googleMap/googleMap.script.js
@@ -0,0 +1,40 @@
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+var map, pos;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 10
+  });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+	 pos = {
+	   lat: position.coords.latitude,
+	   lng: position.coords.longitude
+	 };
+
+	 map.setCenter(pos);
+	 marker = new google.maps.Marker({
+		 position : pos,
+		 map : map
+	 })
+    }, function() {
+	 handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+				    'Error: The Geolocation service failed.' :
+				    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}

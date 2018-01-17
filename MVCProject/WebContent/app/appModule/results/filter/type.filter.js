4fee51e9878386de41a3579e22f30fbb7d144152
Jake <jacobtaric@yahoo.com>
2018-01-17 12:50:05 -0700

    changed dropp downs to not have null values and more

--- MVCProject/WebContent/app/appModule/results/filter/type.filter.js
+++ MVCProject/WebContent/app/appModule/results/filter/type.filter.js
@@ -1,9 +1,13 @@
 angular.module('appModule').filter('typeFilter',function(){
 	return function(resultsList,typeId){
 		var results = [];
-		//console.log(resultsList);
+		
+		//if zero do not filter
+		if(isNaN(parseInt(typeId))){
+			return resultsList;
+		}
+		
 		resultsList.forEach(function(item){
-			//console.log(typeId);
 			if(parseInt(item.company.type.id) === parseInt(typeId)){
 				//console.log('item found');
 				results.push(item);

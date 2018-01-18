e69b1daa8ffa4a76c1da5bffdccf2163c2adbb60
Jake <jacobtaric@yahoo.com>
2018-01-18 09:34:30 -0700

    added active selection to myPage locations

--- MVCProject/WebContent/app/appModule/userProfile/myLocations.component.js
+++ MVCProject/WebContent/app/appModule/userProfile/myLocations.component.js
@@ -1,14 +1,21 @@
 angular.module('appModule').component('myLocations', {
 	templateUrl : 'app/appModule/userProfile/myLocations.component.html',
 	controllerAs : 'vm',
-	controller : function(authService,vetService) {
+	controller : function(authService,vetService,$rootScope) {
 		var vm = this;
 		vm.results = [];
+		vm.active = null;
 		
 		vetService.index().then(function(res){
 			vm.results = res.data;
+			vm.makeActive(vm.results[0]);
 		  }).catch(function(error){
 			  console.log(error);
 		  });
+		
+		vm.makeActive = function(result){
+			vm.active = result;
+			$rootScope.$broadcast('activeSelection', vm.active);
+		}
 	}
 });
